eedcad9d063e31b9592544918d7ecbe5bcfd67de
Charles Berger <charles@thebergers.org.uk>
2019-11-30 17:53:14 +0000

    Added Maven project and updated .gitignore for Eclipse.

---
+++ src/main/java/thebergers/adventofcode2019/App.java
@@ -0,0 +1,13 @@
+package thebergers.adventofcode2019;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
