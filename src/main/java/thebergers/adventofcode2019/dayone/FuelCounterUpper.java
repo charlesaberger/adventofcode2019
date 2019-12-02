2df5f17cfd4592f4cdf576b03b53db551ab031b7
Charles Berger <charles@thebergers.org.uk>
2019-12-02 21:00:02 +0000

    Day One, part two.

--- src/main/java/thebergers/adventofcode2019/dayone/FuelCounterUpper.java
+++ src/main/java/thebergers/adventofcode2019/dayone/FuelCounterUpper.java
@@ -2,6 +2,7 @@ package thebergers.adventofcode2019.dayone;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.function.IntPredicate;
 
 public class FuelCounterUpper {
 
@@ -11,12 +12,34 @@ public class FuelCounterUpper {
 		modules = new LinkedList<>();
 		input.forEach(massStr -> {
 			Integer mass = Integer.parseInt(massStr);
-			modules.add(new Module(mass));
+			Module module = new Module(mass);
+			modules.addAll(calculateAdditionalFuel(module));
 		});
 	}
 
 	public int calculateFuel() {
-		return modules.stream().mapToInt(Module::calculateFuel).sum();
+		IntPredicate greaterThanZero = (x) -> x > 0;
+		return modules.stream()
+				.mapToInt(Module::calculateFuel)
+				.filter(greaterThanZero)
+				.sum();
+	}
+
+	private List<Module> calculateAdditionalFuel(Module firstModule) {
+		LinkedList<Module> extraModules = new LinkedList<>();
+		Module next = firstModule;
+		int fuel = 0;
+		do {
+			fuel = next.calculateFuel();
+			extraModules.add(next);
+			next = new Module(next.calculateFuel());
+		}
+		while (fuel > 0);
+		return extraModules;
+	}
+	
+	public List<Module> getModules() {
+		return modules;
 	}
 	
 }
--- src/main/java/thebergers/adventofcode2019/dayone/Module.java
+++ src/main/java/thebergers/adventofcode2019/dayone/Module.java
@@ -12,5 +12,34 @@ public class Module {
 		return Math.floorDiv(mass, 3) - 2;
 	}
 	
-	
+	public int getMass() {
+		return mass;
+	}
+
+	@Override
+	public String toString() {
+		return "Module [mass=" + mass + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + mass;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Module other = (Module) obj;
+		if (mass != other.mass)
+			return false;
+		return true;
+	}
 }
